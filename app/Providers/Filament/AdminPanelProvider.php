4029be447b7b8afe2090f70939d26be4a70163ed
Curtis Delicata <curtis.delicata@outlook.com>
2024-06-23 19:34:28 +0000

    Configure Filament Shield

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -56,6 +56,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+ ->plugins([
+            \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()
+        ]);
     }
 }
