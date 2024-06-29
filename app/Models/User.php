4932e44da5302a37c20598d3dd275054b46f068c
Curtis Delicata <curtis.delicata@outlook.com>
2024-06-29 15:21:00 +0000

    Configure Jetstream classes in providers and separate panels into Admin and App

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use JoelButcher\Socialstream\HasConnectedAccounts;
 use JoelButcher\Socialstream\SetsProfilePhotoFromUrl;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
@@ -17,14 +18,24 @@ class User extends Authenticatable
 {
     use HasApiTokens;
     use HasConnectedAccounts;
-    use HasFactory;
     use HasRoles;
+    use HasFactory;
     use HasProfilePhoto {
         HasProfilePhoto::profilePhotoUrl as getPhotoUrl;
     }
     use Notifiable;
     use SetsProfilePhotoFromUrl;
     use TwoFactorAuthenticatable;
+    use HasTeams;
+
+    /**
+     * Get the teams the user belongs to.
+     */
+    public function teams()
+    {
+        return $this->belongsToMany(Team::class, 'team_user')->withTimestamps();
+    }
+    use Laravel\Jetstream\HasTeams;
 
     /**
      * The attributes that are mass assignable.
@@ -78,5 +89,13 @@ class User extends Authenticatable
         return filter_var($this->profile_photo_path, FILTER_VALIDATE_URL)
             ? Attribute::get(fn () => $this->profile_photo_path)
             : $this->getPhotoUrl();
+    }
+
+    /**
+     * Get the teams the user owns.
+     */
+    public function ownedTeams()
+    {
+        return $this->hasMany(Team::class);
     }
 }
