8825cd45d3ce8613ddcce3a3ae6180bf76049532
Curtis Delicata <curtis.delicata@outlook.com>
2024-06-29 17:38:04 +0000

    Configure Jetstream classes in providers and separate panels into Admin and App

--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -1,5 +1,4 @@
-@if ($errors->has('socialstream'))
-@elseif ($errors->any())
+@if ($errors->any())
     <div {{ $attributes }}>
         <div class="font-medium text-red-600">{{ __('Whoops! Something went wrong.') }}</div>
 
