f3bbb987047e008d01c1ffaa503f38a238843859
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-20 17:49:26 +0000

    Upgrade composer and npm

---
+++ app/Http/Livewire/CreateTeam.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Actions\Jetstream\CreateTeam;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Jetstream\Http\Livewire\CreateTeamForm;
+
+class CreateTeam extends CreateTeamForm
+{
+    /**
+     * Create a new team.
+     *
+     * @return void
+     */
+    public function createTeam()
+    {
+        $this->validate();
+
+        $team = app(CreateTeam::class)->create(
+            Auth::user(),
+            ['name' => $this->state['name']]
+        );
+
+        return redirect()->route('filament.pages.edit-team', ['team' => $team]);
+    }
+}
