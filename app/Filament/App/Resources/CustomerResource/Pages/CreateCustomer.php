3a2e96ba5cdebc3e084b59481bab49d1a82957c2
Joshua Kisb <joshuakisb@gmail.com>
2024-09-09 11:43:27 +0300

    customer resource

--- app/Filament/App/Resources/CustomerResource/Pages/CreateCustomer.php
+++ app/Filament/App/Resources/CustomerResource/Pages/CreateCustomer.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCustomer extends CreateRecord
 {
     protected static string $resource = CustomerResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
