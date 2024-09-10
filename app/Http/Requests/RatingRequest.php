556a5fc598ae034ffac1d867b5ebb862689dfec0
Joshua Kisb <joshuakisb@gmail.com>
2024-09-10 15:49:05 +0300

    add rating request

---
+++ app/Http/Requests/RatingRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RatingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; 
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'product_id' => 'required|exists:products,id',
+            'overall_rating' => 'required|integer|min:1|max:5',
+            'quality_rating' => 'required|integer|min:1|max:5',
+            'value_rating' => 'required|integer|min:1|max:5',
+            'price_rating' => 'required|integer|min:1|max:5',
+        ];
+    }
+}
