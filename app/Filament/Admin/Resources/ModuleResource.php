146c0f8a71605ec6696611728de38ec936cdabdc
Curtis Delicata <curtis.delicata@outlook.com>
2025-07-25 11:49:53 +0100

    Update ModuleResource.php

--- app/Filament/Admin/Resources/ModuleResource.php
+++ app/Filament/Admin/Resources/ModuleResource.php
@@ -10,6 +10,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Admin\Resources\ModuleResource\Pages;
 
 class ModuleResource extends Resource
 {
