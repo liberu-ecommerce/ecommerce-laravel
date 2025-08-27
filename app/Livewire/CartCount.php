18970b291faae95d25eb07cd6386483be482eb8c
Curtis Delicata <genealogysoftwareuk@gmail.com>
2025-08-27 15:02:29 +0100

    Add CartCount Livewire component

---
+++ app/Livewire/CartCount.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Session;
+
+class CartCount extends Component
+{
+    protected $listeners = ['cartUpdated' => 'render'];
+
+    public function render()
+    {
+        $count = 0;
+        $cart = Session::get('cart', []);
+        
+        foreach ($cart as $item) {
+            $count += $item['quantity'];
+        }
+        
+        return view('livewire.cart-count', [
+            'count' => $count
+        ]);
+    }
+}
