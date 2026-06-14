9f62c2cd206c1702286e70770c1b81c3668c45c9
Raihan Mistry <raihan@codespare.in>
2026-06-14 14:57:22 +0530

    Add new model Permissions

---
+++ app/Models/Permission.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Permission\Models\Permission as SpatiePermission;
+
+class Permission extends SpatiePermission
+{
+    // You can add custom logic or relationships here in the future.
+    // For now, it just inherits everything from Spatie perfectly.
+}
